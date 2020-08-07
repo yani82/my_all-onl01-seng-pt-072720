0337e440930de13ca8f401f1611fd81fccdae62c
Yani Asari <yani.iman1@gmail.com>
2020-08-07 04:17:24 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,9 +4,16 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < 
-  collection.length 
+collection.length 
     block_return_values << 
-  yield(collection[i])
+yield(collection[i])
     i = i + 1 
+  end
+  if
+block_return_values.includes?
+(false)
+    false
+  else
+    true
   end
 end
