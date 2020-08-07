7e59b8e390e62ce5fa5620233fdf0f9fe2663c7e
Yani Asari <yani.iman1@gmail.com>
2020-08-07 04:22:11 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -9,7 +9,6 @@ collection.length
 yield(collection[i])
     i = i + 1 
   end
-  
   if
 block_return_values.include?
 (false)
@@ -17,5 +16,4 @@ block_return_values.include?
   else
     true
   end
-  collection
 end
